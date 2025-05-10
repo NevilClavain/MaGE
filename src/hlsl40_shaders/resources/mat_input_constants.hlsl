ded5dc344d9c399b6bda8f7cfb35a7d16eb387fa
MaNu <emmanuel.chaumont@wanadoo.fr>
2025-05-10 16:59:53 +0200

    inject secondary view stuff to shaders

--- src/hlsl40_shaders/resources/mat_input_constants.hlsl
+++ src/hlsl40_shaders/resources/mat_input_constants.hlsl
@@ -24,6 +24,7 @@
 
 #define matWorldViewProjection                  0
 #define matWorldView                            4
+
 #define matWorld                                8
 
 #define matView                                 12
