8345231b0baae4f21a6e33d8b06b3ae18cf521b1
MaNu <emmanuel.chaumont@wanadoo.fr>
2026-05-07 15:22:50 +0200

    updated

--- src/appbases/src/openenv/openv_init.cpp
+++ src/appbases/src/openenv/openv_init.cpp
@@ -134,8 +134,8 @@ void OpenEnv::d3d11_system_events_openenv()
 					dataCloud->registerData<maths::Real4Vector>("mage.rendering.black_color");
 					dataCloud->updateDataValue<maths::Real4Vector>("mage.rendering.black_color", maths::Real4Vector(0.0, 0.0, 0.0, 1));
 
-					dataCloud->registerData<maths::Real4Vector>("white_color");
-					dataCloud->updateDataValue<maths::Real4Vector>("white_color", maths::Real4Vector(1.0, 1.0, 1.0, 1));
+					dataCloud->registerData<maths::Real4Vector>("mage.rendering.white_color");
+					dataCloud->updateDataValue<maths::Real4Vector>("mage.rendering.white_color", maths::Real4Vector(1.0, 1.0, 1.0, 1));
 
 
 					dataCloud->registerData<maths::Real4Vector>("mage.scene.light0.dir");
